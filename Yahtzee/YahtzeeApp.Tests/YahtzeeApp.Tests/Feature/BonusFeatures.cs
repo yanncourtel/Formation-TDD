7605cb5117dd78ccbdc10f8e7bc85e26e55ceeec
Yann Courtel <courtel.yann@gmail.com>
2022-02-02 10:32:35 -0500

    red: scenario 1 - bonus simple combination

---
+++ Yahtzee/YahtzeeApp.Tests/YahtzeeApp.Tests/Feature/BonusFeatures.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace YahtzeeApp.Tests.Feature
+{
+    public class BonusFeatures
+    {
+        /*
+         *
+Scenario 1 : Bonus Simple Combination
+
+Given the player selects score of 4 for combination ones
+And the player selects score of 8 for combination twos
+And the player selects score of 12 for combination threes
+And the player selects score of 16 for combination fours
+And the player selects score of 20 for combination fives
+When the player selects score of 24 for combination sixes
+Then the simple combination bonus is added to the total score
+And the simple combination bonus score is 35
+         */
+        [Fact]
+        public void Bonus_Simple_Combination()
+        {
+            // Given
+            var scoreBoard = new ScoreBoard();
+            IDiceGenerator diceGenerator = new Mock<IDiceGenerator>();
+            var player = new Player(scoreBoard, diceGenerator);
+
+            SetGeneratedDices(1, 1, 1, 1, 6);
+            player.RollDices();
+            player.SelectScore(Combination.Ones);
+
+            SetGeneratedDices(2, 2, 2, 2, 6);
+            player.RollDices();
+            player.SelectScore(Combination.Twos);
+
+            SetGeneratedDices(3, 3, 3, 3, 6);
+            player.RollDices();
+            player.SelectScore(Combination.Threes);
+
+            SetGeneratedDices(4, 4, 4, 4, 6);
+            player.RollDices();
+            player.SelectScore(Combination.Fours);
+
+            SetGeneratedDices(5, 5, 5, 5, 6);
+            player.RollDices();
+            player.SelectScore(Combination.Fives);
+
+            // When
+            SetGeneratedDices(6, 6, 6, 6, 1);
+            player.RollDices();
+            player.SelectScore(Combination.Sixes);
+
+            // Assert
+            Assert.Equal(119, scoreBoard.GetTotalScore());
+        }
+    }
+}
